830aa08982205b77816c1a3741363fc12018ba93
Pooja Parashuram <83019556+Pooja-p18@users.noreply.github.com>
2026-02-22 21:19:36 +0530

    converted main.ipynb to the main.py
    Refactor data cleaning and clustering processes, and update plotting methods for better visualization.

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 # coding: utf-8
 
 # In[1]:
-
-
 import pandas as pd
 import geopandas as gpd
 import folium
@@ -16,26 +14,18 @@ from shapely.geometry import MultiPoint
 
 
 # In[2]:
-
-
 df = pd.read_csv('data/AIS_2021_01_01.csv')
 
 
 # In[3]:
-
-
 df.size
 
 
 # In[4]:
-
-
 df.head()
 
 
 # In[5]:
-
-
 #drop nulls and duplicates
 df_clean = df.dropna(axis=0, how='any', inplace=False)
 df_clean.drop_duplicates(keep='first', inplace=True)
@@ -43,21 +33,15 @@ df = pd.DataFrame()
 
 
 # In[6]:
-
-
 df_clean.head()
 
 
 # In[7]:
-
-
 #where are these vessels located?
 df_clean.plot.scatter(x='LON', y='LAT', c = 'purple')
 
 
 # In[8]:
-
-
 # Creating dataframe just for very slow/stopped vessels
 
 #very slow is < 0.5 knots
@@ -65,64 +49,44 @@ df_slow = df_clean[df_clean.SOG < 0.5]
 
 
 # In[9]:
-
-
 df_slow.head()
 
 
 # In[10]:
-
-
 df_slow["LAT"].max(), df_slow["LAT"].min()
 
 
 # In[11]:
-
-
 df_slow["LON"].max(), df_slow["LON"].min()
 
 
 # In[12]:
-
-
 df_slow.plot.scatter(x= 'LON', y="LAT", c = 'purple')
 
 
 # In[13]:
-
-
 # check size to see how feasible to plot
 df_slow.shape
 
 
 # In[14]:
-
-
 df_slow_reduced = df_slow[(df_slow["LAT"] < 30) & (df_slow["LAT"] > 20) & (df_slow["LON"] > -90) & (df_slow["LON"] < -70)]
 
 
 # In[15]:
-
-
 df_slow_reduced.shape
 
 
 # In[16]:
-
-
 df_slow_reduced.head()
 
 
 # In[17]:
-
-
 # plot df_slow_reduced
 df_slow_reduced.plot.scatter(x= "LON", y="LAT", c = 'purple')
 
 
 # In[18]:
-
-
 latitude = df_slow["LAT"].mean()
 longitude = df_slow["LON"].mean()
 
@@ -131,18 +95,13 @@ DFW_Map
 
 
 # In[25]:
-
-
 # Pick location for folium map
 latitude = 26
 Longitude = -80
 
 
 # In[26]:
-
-
 # map vessels with folium--- use VesselName as label
-
 slow_vessels = folium.map.FeatureGroup()
 latitudes = list(df_slow_reduced.LAT)
 longitudes = list(df_slow_reduced.LON)
@@ -156,22 +115,15 @@ for Lat, Lon, labels in zip(latitudes, longitudes, labels):
 
 
 # In[27]:
-
-
 DFW_Map
 
 
 # In[28]:
-
-
 # CLUSTERING - DBSCAN
-
 df_slow_reduced.head()
 
 
 # In[29]:
-
-
 import pandas as pd, numpy as np, matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 from geopy.distance import great_circle
@@ -180,8 +132,6 @@ coords = df_slow_reduced[['LAT', 'LON']].to_numpy()
 
 
 # In[30]:
-
-
 # first DBSCAN with epsilon = 1.5km
 kms_per_radian = 6371.0088
 epsilon = 1.5 / kms_per_radian
@@ -193,22 +143,16 @@ print('Number of clusters: {}'.format(num_clusters))
 
 
 # In[32]:
-
-
 df_slow_reduced["label"] = cluster_labels
 
 
 # In[33]:
-
-
 # plot label #1
 fg = sns.FacetGrid(data=df_slow_reduced, hue="label", height=6, aspect=1.61)
 fg.map(plt.scatter, 'LAT', 'LON').add_legend()
 
 
 # In[34]:
-
-
 kms_per_radian = 6371.0088
 epsilon = 20 / kms_per_radian
 db = DBSCAN(eps=epsilon, min_samples=1, algorithm='ball_tree', metric='haversine').fit(np.radians(coords))
@@ -219,8 +163,6 @@ print('Number of clusters: {}'.format(num_clusters))
 
 
 # In[37]:
-
-
 # plot label 2
 df_slow_reduced["label2"] = cluster_labels
 
@@ -229,8 +171,6 @@ fg.map(plt.scatter, 'LAT', 'LON').add_legend()
 
 
 # In[41]:
-
-
 kms_per_radian = 6371.0088
 epsilon = 100 / kms_per_radian
 db = DBSCAN(eps=epsilon, min_samples=1, algorithm='ball_tree', metric='haversine').fit(np.radians(coords))
@@ -241,22 +181,16 @@ print('Number of clusters: {}'.format(num_clusters))
 
 
 # In[42]:
-
-
 df_slow_reduced["label3"] = cluster_labels
 
 
 # In[43]:
-
-
 # plot label 3
 fg = sns.FacetGrid(data=df_slow_reduced, hue='label3', aspect=1.61)
 fg.map(plt.scatter, 'LAT', 'LON').add_legend()
 
 
 # In[44]:
-
-
 df_sample = df.head(5000)
 df_sample.to_csv("data/sample.csv", index=False)
 
